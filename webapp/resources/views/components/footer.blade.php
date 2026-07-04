8c9f57c0b141ae284aa045f356e94f16fe0230a3
n1k1tal0x <d.cuzya07@gmail.com>
2026-07-04 13:39:04 +0300

    вынес css

--- webapp/resources/views/components/footer.blade.php
+++ webapp/resources/views/components/footer.blade.php
@@ -1,3 +1,5 @@
+@vite(["resources/css/footer.css"])
+
 <div class="Footer">
     <div class="FooterContent">
         <span>© 2023 — 2412 «Галактический вестник»</span>
--- webapp/resources/views/components/header.blade.php
+++ webapp/resources/views/components/header.blade.php
@@ -1,3 +1,5 @@
+@vite(["resources/css/header.css"])
+
 <div class="Header">
     <img src="/logo.png" alt="logo" srcset="">
     <h1>Галактический вестник</h1>
