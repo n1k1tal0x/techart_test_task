ef94a412c212da6616ca1ebe737eae5b5c2b1de5
n1k1tal0x <d.cuzya07@gmail.com>
2026-07-06 09:09:10 +0300

    Сортировка по дате

--- webapp/app/Http/Controllers/NewsController.php
+++ webapp/app/Http/Controllers/NewsController.php
@@ -9,7 +9,7 @@ class NewsController extends Controller
 {
     public function index(Request $request)
     {
-        $news = News::orderByDesc('id')->get();
+        $news = News::orderByDesc('date')->get();
         $page = $request->integer('page', 0);
 
         return view('index', compact('news', 'page'));
