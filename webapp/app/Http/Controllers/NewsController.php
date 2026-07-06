b636b1e1d03c6725cdcf54dab53664188fe59e21
n1k1tal0x <d.cuzya07@gmail.com>
2026-07-06 08:24:22 +0300

    Добавил переключение страниц

--- webapp/app/Http/Controllers/NewsController.php
+++ webapp/app/Http/Controllers/NewsController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\News;
+use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $news = News::orderByDesc('id')->get();
+        $page = $request->integer('page', 0);
 
-        return view('index', compact('news'));
+        return view('index', compact('news', 'page'));
     }
 }
