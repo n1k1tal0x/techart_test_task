d7f82bdcb8907878638d67fdf61354a8d40061ae
n1k1tal0x <d.cuzya07@gmail.com>
2026-07-07 13:52:50 +0300

    fix image

--- webapp/resources/views/index.blade.php
+++ webapp/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="BestNewBlock" style="background-image: url('images/{{ $news->first()->image }}')">
+    <div class="BestNewBlock" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('images/{{ $news->first()->image }}');">
         <h1>{{ $news->first()->title }}</h1>
         {!!  $news->first()->announce !!}
     </div>
@@ -39,7 +39,7 @@
 <path d="M1 6.36401C0.447715 6.36401 -4.82823e-08 6.81173 0 7.36401C4.82823e-08 7.9163 0.447715 8.36401 1 8.36401L1 6.36401ZM16.466 8.07112C16.8565 7.68059 16.8565 7.04743 16.466 6.65691L10.102 0.292945C9.7115 -0.0975793 9.07834 -0.0975792 8.68781 0.292945C8.29729 0.68347 8.29729 1.31663 8.68781 1.70716L14.3447 7.36401L8.68781 13.0209C8.29729 13.4114 8.29729 14.0446 8.68781 14.4351C9.07834 14.8256 9.7115 14.8256 10.102 14.4351L16.466 8.07112ZM1 8.36401L15.7589 8.36401L15.7589 6.36401L1 6.36401L1 8.36401Z" fill="#841844"/>
 </svg>
                     </span>
-                </div>    
+                </div>
                 @endif
             </div>
         </div>
